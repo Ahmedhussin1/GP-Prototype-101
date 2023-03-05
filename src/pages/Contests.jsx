7620143760e99171c57a840a1e2ede5f9bbdd2c5
YE <ye@pop-os.localdomain>
2023-03-06 00:15:45 +0200

    adding the search to the contest page

--- src/pages/Contests.jsx
+++ src/pages/Contests.jsx
@@ -1,45 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Cards from '../component/Cards'
 import { posts } from '../data'
 function Contests() {
+  const [contestName, setContestName] = useState("");
+
 	return (
-		<div className="overflow-auto rounded-lg shadow container mx-auto max-w-7xl">
-			<table className="w-full">
-				<thead className="bg-gray-50 border-b-2 border-gray-200">
-					<tr>
-						<th className="w-20 p-3 text-sm font-semibold tracking-wide text-left">ID</th>
-						<th className="p-3 text-sm font-semibold tracking-wide text-left">Name</th>
-						<th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">Difficulty</th>
-						<th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">length</th>
-					</tr>
-				</thead>
-				{posts.map((post) => (
-					<tbody>
-						<tr className="bg-white">
-							<td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-								<Link key={post.id} to={`/problems/${post.id}`}>
-									{post.id}
-								</Link>
-							</td>
-							<td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-								<Link key={post.id} to={`/problems/${post.id}`}>
-									<div className="flex flex-col md:flex-row justify-between">
-										<div>{post.title}</div>
-									</div>
-								</Link>
-							</td>
-							<td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-								<span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">{post.diff}</span>
-							</td>
-							<td className="p-3 text-sm text-gray-700 whitespace-nowrap ">{post.length}</td>
-						</tr>
-						<Link key={post.id} to={`/problems/${post.id}`}></Link>
-					</tbody>
-				))}
-			</table>
-		</div>
-	)
+    <div className="overflow-auto rounded-lg shadow container mx-auto max-w-7xl">
+      <div>
+        <input
+          className="border-2 rounded-md border-black"
+          placeholder="search"
+          onChange={(e) => setContestName(e.target.value)}
+        ></input>
+      </div>
+      <table className="w-full">
+        <thead className="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            <th className="w-20 p-3 text-sm font-semibold tracking-wide text-left">
+              ID
+            </th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">
+              Name
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Difficulty
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              length
+            </th>
+          </tr>
+        </thead>
+        {posts
+          .filter((contest) =>
+            contest.title.toLowerCase().includes(contestName)
+          )
+          .map((post) => (
+            <tbody>
+              <tr className="bg-white">
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <Link key={post.id} to={`/problems/${post.id}`}>
+                    {post.id}
+                  </Link>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <Link key={post.id} to={`/problems/${post.id}`}>
+                    <div className="flex flex-col md:flex-row justify-between">
+                      <div>{post.title}</div>
+                    </div>
+                  </Link>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
+                    {post.diff}
+                  </span>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap ">
+                  {post.length}
+                </td>
+              </tr>
+              <Link key={post.id} to={`/problems/${post.id}`}></Link>
+            </tbody>
+          ))}
+      </table>
+    </div>
+  );
 }
 
 export default Contests
--- src/problemsData.js
+++ src/problemsData.js
@@ -36,7 +36,7 @@ export const problemsData = [
             inputStatement:
                 "The first line contains one integer t(1≤t≤1000) — the number of test cases.The first line of each test case contains two integers n and k(1≤n,k≤50).Then n lines follow, i-th line of them contains two integers li and ri(1≤li,ri≤50; li≤ri) — the endpoints of the i-th segment.",
             outputStatement:
-                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).";
+                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).",
         },
     },
     {
@@ -56,7 +56,7 @@ export const problemsData = [
             inputStatement:
                 "The first line contains one integer t(1≤t≤1000) — the number of test cases.The first line of each test case contains two integers n and k(1≤n,k≤50).Then n lines follow, i-th line of them contains two integers li and ri(1≤li,ri≤50; li≤ri) — the endpoints of the i-th segment.",
             outputStatement:
-                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).";
+                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).",
         },
     },
     {
@@ -76,7 +76,7 @@ export const problemsData = [
             inputStatement:
                 "The first line contains one integer t(1≤t≤1000) — the number of test cases.The first line of each test case contains two integers n and k(1≤n,k≤50).Then n lines follow, i-th line of them contains two integers li and ri(1≤li,ri≤50; li≤ri) — the endpoints of the i-th segment.",
             outputStatement:
-                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).";
+                "For each test case, print YES if it is possible to remove some (possibly zero) segments, so that the given point k becomes ideal, otherwise print NO.You may print each letter in any case (YES, yes, Yes will all be recognized as positive answer, NO, no and nO will all be recognized as negative answer).",
         },
     },
 ];
